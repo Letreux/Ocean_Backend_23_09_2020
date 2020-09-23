7309d86c86a848a61364d780f5bbdc9dcf5f524c
Letreux <leodiashgbr@gmail.com>
2020-09-23 17:33:52 -0300

    Tudo

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectID
+
+// Criando um bloco assíncrono e já executando esse bloco
+(async () => {
+
+
+const connectionString = 'mongodb+srv://Letreux:7ync166yPZ0bg4iE@cluster0.q5h9c.mongodb.net/Ocean_MongoDB?retryWrites=true&w=majority'
+
+// async/await
+
+console.info('Conectando ao banco de dados');
+
+const client = await mongodb.MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+})
+
+console.info('Conectado ao banco de dados');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,29 +33,64 @@ app.get('/', (req, res) => {
 // CRUD -> Create, Read (Read All e Read Single), Update and Delete
 // CRUD -> Criar, Ler (Ler tudo e ler individualmente), atualizar e remover
 
-const mensagens = [];
+const db = await client.db('Ocean_MongoDB')
+
+const mensagens = await db.collection('Mensagens');
 
 // Read All
-app.get('/mensagens', (req, res) => {
-    res.json(mensagens.filter(Boolean));
+app.get('/mensagens', async (req, res) => {
+const findResult = await mensagens.find({}).toArray();
+
+    res.json(findResult);
 });
 
 // Create
-app.post('/mensagens', (req, res) => {
+app.post('/mensagens', async (req, res) => {
+    const mensagem = req.body
+
+    const resultado = await mensagens.insertOne(mensagem)
+
+    const objetoInserido = resultado.ops[0]
+
+    res.json(objetoInserido)
 });
 
 // Read Single
-app.get('/mensagens/:id', (req, res) => {
+app.get('/mensagens/:id', async (req, res) => {
+    const id = req.params.id
+
+    const mensagem = await mensagens.findOne({ _id: ObjectId(id) })
+
+    res.json(mensagem)
 });
 
 // Update
-app.put('/mensagens/:id', (req, res) => {
+app.put('/mensagens/:id', async (req, res) => {
+    const id = req.params.id
+
+    const novaMensagem = req.body
+
+    const mensagemAtual = await mensagens.findOne({ _id: ObjectId(id) })
+
+    mensagemAtual.texto = novaMensagem.texto
+
+    const resultado = await mensagens.updateOne({ _id: ObjectId(id) }, { $set: mensagemAtual })
+
+    res.json(mensagemAtual)
 });
 
 // Delete
-app.delete('/mensagens/:id', (req, res) => {
+app.delete('/mensagens/:id', async (req, res) => {
+    const id = req.params.id
+
+    const resultado = await mensagens.deleteOne({ _id: mongodb.ObjectID(id) })
+
+    res.send(`Mensagem ${id} removida com sucesso.`)
 });
 
 app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 });
+
+// Fecho o bloco assíncrono
+})();
